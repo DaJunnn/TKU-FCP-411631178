7ce81d8fa25615c7cb085c58b94825cd7f1b33c0
TKU-AI-411631178 <113398975+DaJunnn@users.noreply.github.com>
2024-07-03 14:16:44 +0800

    hm

---
+++ hm_02.java
@@ -0,0 +1,14 @@
+import javax.print.DocFlavor.INPUT_STREAM;
+
+public class hm_02 {
+    public static void main(String[] args) {
+        String name="蔡大鈞";
+        int age=20;
+        String hobby="打電腦";
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(hobby);
+
+
+    }
+}
---
+++ hm_04.java
@@ -0,0 +1,16 @@
+public class hm_04 {
+
+    public static void main(String[] args) {
+        int a=10;
+        int b=5, sum1,sum2,sum3,sum4;
+        sum1=a+b;
+        sum2=a-b;
+        sum3=a*b;
+        sum4=a/b;
+        System.out.println("和="+sum1);
+        System.out.println("差="+sum2);
+        System.out.println("積="+sum3);
+        System.out.println("商="+sum4);
+    }
+
+}
